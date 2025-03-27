18aba04bc014482f8a16ac3a608152613132d554
Dongxu Wang <echowdx@gmail.com>
2025-03-27 16:23:54 +0100

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,11 +32,15 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<&str, Command>) -> Vec<&str> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<&str> = vec![];
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim(),
+                Command::Append(x) => String::from(string) + ("bar" * x),
+            }
         }
         output
     }
@@ -45,7 +49,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module;
     use super::Command;
 
     #[test]
